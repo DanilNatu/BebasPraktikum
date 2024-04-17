690f3dd93c291979d79c2b35343bc1f146589eb8
DanilNatu <natudenilson@gmail.com>
2024-04-17 12:00:55 +0800

    Praktikum main.js

---
+++ main.js
@@ -0,0 +1,101 @@
+//alert('Hai Kamu')
+
+//console.log("hi")
+
+//prompt('masukan usia anda')
+
+//==
+//var usia = 26
+//console.log('Hai Kamu, usia kamu adalah ' + usia)
+
+//===
+//const nilai tidak bisa diubah
+//let nilai bisa diubah
+//const usia = 30
+//let usia = 30
+//usia = 35
+//console.log('Hai Kamu, usia kamu adalah ' + usia)
+//alert('Hai Kamu, usia kamu adalah ' + usia)
+
+//combine dengan promt
+//let usia = prompt('berapa usia kamu?')
+//alert ('usia kamu adalah ' + usia)
+
+// === tipe data
+//let nama = 'Danil natu' //string
+//let usia = 19 //integer number
+//let tinggiBadan = 172 //float number
+//let hobi = ['Basket', 'Ngoding'] //array of string
+//let tempatLahir = {provinsi: 'NTT',kota:'Kefa'} //
+//let beratBadan
+//let pacar = null
+//console.log('Halo, usia kamu adalah ' + usia, 'nama saya ' + nama, 'tinggi badan saya ' + tinggiBadan, 'hobi saya ' + hobi, 'saya lahir di ' + tempatLahir.kota)
+//alert (`nama saya ${nama} usia saya ${usia} tinggi badan${tinggiBadan}, hobi${hobi} tempat lahir ${tempatLahir.kota}, pacar ${pacar}`)
+
+// let nama = 'Danil natu'; //string
+// let usia = 19; //integer number
+// let tinggiBadan = 172; //float number
+// let hobi = ['Basket', 'Ngoding']; //array of string
+// let tempatLahir = {provinsi: 'NTT',kota:'Kefa'}; //
+// let beratBadan;
+// let pacar = 2;
+
+// if (pacar == null){
+//     pacar = 'belum punya'
+// } else {
+//     pacar = 'udah punya'
+// }
+
+// console.log('Halo, usia kamu adalah ' + usia, 'nama saya ' + nama, 'tinggi badan saya ' + tinggiBadan, 'hobi saya ' + hobi, 'saya lahir di ' + tempatLahir.kota, 'pacar saya ' + pacar)
+
+// switch (pacar) {
+//     case 1:
+//         pacar = "punya 1 aja";
+//         break;
+//     case 2:
+//         pacar = "punya 2, aku cukup playboy";
+//         break;
+//     default:
+//         pacar = "belum punya pacar";
+//         break;
+// }
+
+// console.log('Halo, usia kamu adalah ' + usia, 'nama saya ' + nama, 'tinggi badan saya ' + tinggiBadan, 'hobi saya ' + hobi, 'saya lahir di ' + tempatLahir.kota, 'pacar saya ' + pacar)
+
+// let saldoAwal = 50000
+// let saldoTambahan = 100000
+// const hutang = 30000
+// const saldoAkhir = saldoAwal + saldoTambahan - hutang
+
+// alert (`saya memiliki saldo awal Rp${saldoAwal}, dan akan mendapatkan gaji dengan saldo tambahan sejumlah Rp${saldoTambahan}, jadi total saldo yang saya miliki sebanyak Rp.${saldoAkhir}`);
+
+// const x = 20
+// const y = 3
+// const z = x * y
+
+//===array
+// let namaMahasiswa = ['Dagdag', 'Kali', 'Supri']
+// namaMahasiswa.push('cinta', 'bakar')
+// namaMahasiswa.shift() //menghapus value data pertama
+// namaMahasiswa.pop() //menghapus value data terakhir
+// alert (namaMahasiswa)
+
+// === for loop
+// for (let i = 1; i <= 10; ++i){
+//     console.log('dasar javascript')
+// }
+
+// const namaMahasiswa=['Dagdag', 'Kali', 'Supri', 'Cinta', 'Bakar'];
+// for (let i = 0; i < namaMahasiswa.length; ++i){
+//     console.log (`NIM :${i+1} Nama : ${namaMahasiswa[i]}`);
+// }
+
+// === while
+// let i = 0
+// while (i < 5){
+//     i++
+//     console.log('dasar javascript')
+// }
+
+
+
